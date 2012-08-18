0daa757ae7a86f5647dc82bb17962d633f1bbde7
Dmitri Cherniak <dmitric@gmail.com>
2012-08-18 14:23:42 -0400

    Initial Commit

---
+++ DLCImagePickerController/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  DLCImagePickerController
+//
+//  Created by Dmitri Cherniak on 8/18/12.
+//  Copyright (c) 2012 DLC Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ DLCImagePickerController/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  DLCImagePickerController
+//
+//  Created by Dmitri Cherniak on 8/18/12.
+//  Copyright (c) 2012 DLC Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
